7e51bdf4e4793dd1a1004fb7cb5c52bb107da1a8
Camila Nunes <cvn.camila@gmail.com>
2024-02-02 19:44:25 -0300

    modificando api;

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'https://localhost:55759' // Substitua pela URL do seu backend
-  baseURL: 'https://camilanunes-001-site2.ctempurl.com/'
+  baseURL: 'http://camilanunes-001-site3.ctempurl.com/'
 });
 
 export default api; 
