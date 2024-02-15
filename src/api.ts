c239e482e295ba5dad71f8a8257d90c83867b0bf
Camila Nunes <cvn.camila@gmail.com>
2024-02-15 17:35:33 -0300

    Merge;

--- src/api.ts
+++ src/api.ts
@@ -2,14 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'SUA_BASE_URL_AQUI',
-<<<<<<< HEAD
   //baseURL: 'https://localhost:55759' // Substitua pela URL do seu backend
-  //baseURL: 'https://camilanunes-001-site3.ctempurl.com'
-  baseURL: 'https://nuersescales.com.br/'
-=======
-  baseURL: 'https://localhost:55759' // Substitua pela URL do seu backend
-  //baseURL: 'https://camilanunes-001-site3.ctempurl.com'
->>>>>>> 7d24a78 (conexão para dev;)
+  baseURL: 'https://camilanunes-001-site3.ctempurl.com'
 });
 
 api.interceptors.request.use((config) => {
