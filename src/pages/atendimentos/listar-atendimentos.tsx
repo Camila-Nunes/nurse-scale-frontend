0b8066b5e8454c0f0dae0e119b4bc7fdafdf9750
Camila Nunes <cvn.camila@gmail.com>
2024-04-12 11:18:00 -0300

    melhorando os filtros;

--- src/pages/atendimentos/listar-atendimentos.tsx
+++ src/pages/atendimentos/listar-atendimentos.tsx
@@ -409,14 +409,14 @@ const ListarAtendimentos: React.FC<ListarAtendimentosProps> = ({ meses }) => {
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="statusAtendimento" className="block text-sm font-medium leading-6 text-gray-900">Status</label>
+              <label htmlFor="StatusAtendimento" className="block text-sm font-medium leading-6 text-gray-900">Status</label>
               <div className="mt-2">
                 <select
-                    id="statusAtendimento"
-                    name="statusAtendimento"
+                    id="StatusAtendimento"
+                    name="StatusAtendimento"
                     value={StatusAtendimento}
                     onChange={handleFilterChange}
-                    autoComplete="statusAtendimento"
+                    autoComplete="StatusAtendimento"
                     className="block w-full rounded-md border-0 py-2 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                   >
                     <option value="">Status</option>
