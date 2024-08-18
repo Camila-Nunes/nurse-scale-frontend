4ab0da42a77c952c2815342fddbc34933fef6473
Camila Nunes <cvn.camila@gmail.com>
2024-08-18 14:55:50 -0300

    baseURL;

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://camilanunes-001-site1.ctempurl.com'
-  //baseURL: 'https://localhost:55759/'
 });
 
 // api.interceptors.request.use((config) => {
