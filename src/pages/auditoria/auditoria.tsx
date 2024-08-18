081cb88b4270d59ac48a2ca59cc8a324174befca
Camila Nunes <cvn.camila@gmail.com>
2024-08-18 14:52:21 -0300

    carregando dados;

--- src/pages/auditoria/auditoria.tsx
+++ src/pages/auditoria/auditoria.tsx
@@ -83,51 +83,63 @@ const Auditoria: React.FC<AuditoriaProps> = ({ meses }) => {
           <AnoSelect onSelectYear={handleSelectYear} />
         </div>
         <div className="mt-6 mx-auto pt-4 shadow rounded-md bg-slate-50">
-          <div className="mt-6 overflow-auto rounded-lg shadow hidden md:block">
-            <table className="w-full">
-              <thead className="text-left text-white border-b-2 border-gray-200 bg-teal-600">
-                <tr>
-                  <th className="p-3 text-sm font-semibold tracking-wide ext-left uppercase">Profissional</th>
-                  <th className="p-3 text-sm font-semibold tracking-wide ext-left uppercase">Mês</th>
-                  <th className="p-3 text-sm font-semibold tracking-wide ext-left text-right uppercase">Atendimentos Realizados</th>
-                  <th className="p-3 text-sm font-semibold tracking-wide ext-left text-right uppercase">Saldo Devedor ao Profissional</th>
-                  <th className="p-3 text-sm font-semibold tracking-wide ext-left text-right uppercase">Checagem</th>
-                  <th className="p-3 text-sm font-semibold tracking-wide ext-left text-right uppercase">Valor Antecipado</th>
-                  <th className="p-3 text-sm font-semibold tracking-wide ext-left text-right uppercase">Saldo Devedor Atual ao Profissional</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-100">
-                {resumoAuditoriaEmpresas.map((resumoAuditoriaEmpresa: any, index: number) => (
-                <tr key={index} className="border-b border-gray-200">
-                  <td className="font-semibold text-left w-96 p-3 text-sm text-gray-700 whitespace-nowrap">{resumoAuditoriaEmpresa.profissional}</td>
-                  <td className="font-semibold text-left w-10 p-3 text-sm text-gray-700 whitespace-nowrap uppercase">{resumoAuditoriaEmpresa.nomeMes}</td>
-                  <td className="font-semibold text-right w-80 p-3 text-sm text-gray-700 whitespace-nowrap">{resumoAuditoriaEmpresa.atendimentosRealizados}</td>
-                  <td className="font-semibold text-right w-80 p-3 text-sm text-gray-700 whitespace-nowrap">
-                    <span className={`inline-block px-4 py-1 font-semibold rounded ${isNaN(parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissional)) ? 'bg-red-500 text-white' : 'bg-blue-800 text-white'}`}>
-                      R$ {isNaN(parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissional)) ? 'Valor inválido' : parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissional).toFixed(2)}
-                    </span>
-                  </td>
-                  <td className="font-semibold text-right w-80 p-3 text-sm text-gray-700 whitespace-nowrap">
-                    <span className={`inline-block px-4 py-1 font-semibold rounded ${isNaN(parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissional)) ? 'bg-red-500 text-white' : 'bg-blue-600 text-white'}`}>
-                      R$ {isNaN(parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissional)) ? 'Valor inválido' : parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissional).toFixed(2)}
-                    </span>
-                  </td>
-                  <td className="font-semibold text-right w-96 p-3 text-sm text-gray-700 whitespace-nowrap">
-                    <span className={`inline-block px-4 py-1 font-semibold rounded ${isNaN(parseFloat(resumoAuditoriaEmpresa.valorAdiantado)) ? 'bg-red-500 text-white' : 'bg-green-800 text-white'}`}>
-                      R$ {isNaN(parseFloat(resumoAuditoriaEmpresa.valorAdiantado)) ? 'Valor inválido' : parseFloat(resumoAuditoriaEmpresa.valorAdiantado).toFixed(2)}
-                    </span>
-                  </td>
-                  <td className="font-semibold text-right w-96 p-3 text-sm text-gray-700 whitespace-nowrap">
-                    <span className={`inline-block px-4 py-1 font-semibold rounded ${isNaN(parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissionalAtual)) ? 'bg-red-500 text-white' : 'bg-red-800 text-white'}`}>
-                      R$ {isNaN(parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissionalAtual)) ? 'Valor inválido' : parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissionalAtual).toFixed(2)}
-                    </span>
-                  </td>    
-                </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+          {resumoAuditoriaEmpresas.length > 0 ? (
+            <div className="mt-6 overflow-auto rounded-lg shadow hidden md:block">
+              <table className="w-full">
+                <thead className="text-left text-white border-b-2 border-gray-200 bg-teal-600">
+                  <tr>
+                    <th className="p-3 text-sm font-semibold tracking-wide text-left uppercase">Profissional</th>
+                    <th className="p-3 text-sm font-semibold tracking-wide text-left uppercase">Mês</th>
+                    <th className="p-3 text-sm font-semibold tracking-wide text-right uppercase">Atendimentos Realizados</th>
+                    <th className="p-3 text-sm font-semibold tracking-wide text-right uppercase">Saldo Devedor ao Profissional</th>
+                    <th className="p-3 text-sm font-semibold tracking-wide text-right uppercase">Checagem</th>
+                    <th className="p-3 text-sm font-semibold tracking-wide text-right uppercase">Valor Antecipado</th>
+                    <th className="p-3 text-sm font-semibold tracking-wide text-right uppercase">Saldo Devedor Atual ao Profissional</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-100">
+                  {resumoAuditoriaEmpresas.map((resumoAuditoriaEmpresa: any, index: number) => (
+                    <tr key={index} className="border-b border-gray-200">
+                      <td className="font-semibold text-left w-96 p-3 text-sm text-gray-700 whitespace-nowrap">
+                        {resumoAuditoriaEmpresa.profissional}
+                      </td>
+                      <td className="font-semibold text-left w-10 p-3 text-sm text-gray-700 whitespace-nowrap uppercase">
+                        {resumoAuditoriaEmpresa.nomeMes}
+                      </td>
+                      <td className="font-semibold text-right w-80 p-3 text-sm text-gray-700 whitespace-nowrap">
+                        {resumoAuditoriaEmpresa.atendimentosRealizados}
+                      </td>
+                      <td className="font-semibold text-right w-80 p-3 text-sm text-gray-700 whitespace-nowrap">
+                        <span className={`inline-block px-4 py-1 font-semibold rounded ${isNaN(parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissional)) ? 'bg-red-500 text-white' : 'bg-blue-800 text-white'}`}>
+                          R$ {isNaN(parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissional)) ? 'Valor inválido' : parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissional).toFixed(2)}
+                        </span>
+                      </td>
+                      <td className="font-semibold text-right w-80 p-3 text-sm text-gray-700 whitespace-nowrap">
+                        <span className={`inline-block px-4 py-1 font-semibold rounded ${isNaN(parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissional)) ? 'bg-red-500 text-white' : 'bg-blue-600 text-white'}`}>
+                          R$ {isNaN(parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissional)) ? 'Valor inválido' : parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissional).toFixed(2)}
+                        </span>
+                      </td>
+                      <td className="font-semibold text-right w-96 p-3 text-sm text-gray-700 whitespace-nowrap">
+                        <span className={`inline-block px-4 py-1 font-semibold rounded ${isNaN(parseFloat(resumoAuditoriaEmpresa.valorAdiantado)) ? 'bg-red-500 text-white' : 'bg-green-800 text-white'}`}>
+                          R$ {isNaN(parseFloat(resumoAuditoriaEmpresa.valorAdiantado)) ? 'Valor inválido' : parseFloat(resumoAuditoriaEmpresa.valorAdiantado).toFixed(2)}
+                        </span>
+                      </td>
+                      <td className="font-semibold text-right w-96 p-3 text-sm text-gray-700 whitespace-nowrap">
+                        <span className={`inline-block px-4 py-1 font-semibold rounded ${isNaN(parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissionalAtual)) ? 'bg-red-500 text-white' : 'bg-red-800 text-white'}`}>
+                          R$ {isNaN(parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissionalAtual)) ? 'Valor inválido' : parseFloat(resumoAuditoriaEmpresa.saldoDevedorProfissionalAtual).toFixed(2)}
+                        </span>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            <div className="p-6 text-center text-gray-600">
+              Não há dados a serem mostrados.
+            </div>
+          )}
         </div>
       </form>
     </Page>
